e3983e2082e8fb6cc5a842c6a26edaf2c5157eea
SRINIVASAN Thulasi Ram - CIEC <thulasiram.srinivasan@skytv.it>
2025-08-16 18:39:58 +0530

    made small changes

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
     public static void main(String[] args) {
+        System.out.println("Starting User Service Application..."); // 👈 Add this
         SpringApplication.run(UserServiceApplication.class, args);
     }
 }
